f86cf8ed7fbe60bb0b070a4c25fdbbef97e5e9b6
James Lee <jamesjlee8@gmail.com>
2014-07-11 17:26:16 -0400

    Changed name again...

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'James Lee' });
+  res.render('index', { title: 'James Joosung Lee' });
 });
 
 module.exports = router;
